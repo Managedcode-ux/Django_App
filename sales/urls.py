0c6c5e7ba063f951768c7c9808338c80dd77e86d
Mayank <mayank.mehra.dev@gmail.com>
2021-06-30 19:12:00 +0530

    Created Sales Views

--- sales/urls.py
+++ sales/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from django.urls.resolvers import URLPattern 
-from .views import home_view
+from .views import home_view, SalesListView
 
 app_name = 'sales'
 
 urlpatterns = [
     path('', home_view, name = 'home'),
+    path('list/', SalesListView.as_view(), name='list')
 ]
